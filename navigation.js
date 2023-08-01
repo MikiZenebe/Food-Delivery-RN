95d6f2b7f7ae33357810ba5d2bb106030c90ce95
Mikiyas Zenebe <mikizenebe10@gmail.com>
2023-08-01 23:05:22 +0300

    updated

--- navigation.js
+++ navigation.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
-import RestaurantScreen from "./screens/RestaurantSCreen";
+import RestaurantScreen from "./screens/RestaurantScreen";
 
 const Stack = createNativeStackNavigator();
 
